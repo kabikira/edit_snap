0c8081f9fa6849607f41d7902fd8d42b02b8387e
kabikira <kabikirar2@protonmail.com>
2024-11-12 09:43:55 +0900

    写真選択

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:edit_snap/start_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +12,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: L10n.localizationsDelegates,
+      supportedLocales: L10n.supportedLocales,
       title: 'Edit Snap',
       theme: ThemeData(
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: const StartScreen(),
     );
   }
 }
